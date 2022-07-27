7900f5225873716bc386613774f83a228388d557
Sam <sam.osborne159@gmail.com>
2022-07-26 23:04:39 -0700

    footer and guide

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import recentRequests from "../components/recentRequests";
 
 const Home: NextPage = (props) => {
   return (
-    <div>
+    <div className="min-h-screen">
       {header()}
       <div className="md:grid md:grid-cols-3">
         {/* <!-- left column --> */}
@@ -77,6 +77,27 @@ const Home: NextPage = (props) => {
                 </div>
               </div>
             </a>
+            <a href="/setup" className="flex justify-end pt-2">
+              <div className="text-primary-300">
+                Guide: Put books on your Kindle
+              </div>
+              <div className="place-self-center pl-1">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-4 w-4"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+                  />
+                </svg>
+              </div>
+            </a>
           </div>
         </div>
 
@@ -98,6 +119,12 @@ const Home: NextPage = (props) => {
           <div>{recentRequests()}</div>
         </div>
       </div>
+      <footer className="sticky top-[100vh] flex items-center justify-between rounded-lg bg-white p-4 shadow md:p-6">
+        <span className="text-sm text-primary-200">For friends and family</span>
+        <span className="text-sm text-primary-200">
+          <a href="http://bookshelfadmin.jk">Admin</a>
+        </span>
+      </footer>
     </div>
   );
 };
--- pages/setup.tsx
+++ pages/setup.tsx
@@ -8,6 +8,27 @@ const Setup: NextPage = (props) => {
       {header()}
       <div className="relative flex min-h-screen flex-col justify-center overflow-hidden break-words bg-gray-50 py-8 lg:py-12">
         <article>
+          <div className="relative w-full p-2 md:mx-auto md:max-w-3xl lg:max-w-4xl">
+            <a href="/">
+              <div className="flex text-primary-500">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M11 17l-5-5m0 0l5-5m-5 5h12"
+                />
+              </svg>
+              <span>Back to home</span>
+              </div>
+            </a>
+          </div>
           <div className="relative w-full rounded-xl bg-white px-6 py-12 shadow-xl shadow-slate-700/10 ring-1 ring-gray-900/5 md:mx-auto md:max-w-3xl lg:max-w-4xl lg:pt-16 lg:pb-28">
             <div className="prose prose-sm mx-auto md:prose-base">
               <SetupMarkdown />
