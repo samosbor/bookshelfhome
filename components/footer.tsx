ed41073ead11bf52ade3c3dbeb165eb42e1c9013
Sam <sam.osborne159@gmail.com>
2022-08-08 21:31:24 -0700

    kindle link

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,7 +2,7 @@ export default function footer() {
   return (
     <footer className="sticky top-full flex items-center justify-between rounded-t-lg bg-white p-4 shadow md:p-6">
       <span className="text-sm text-primary-200">
-        <a href="https://www.woot.com/plus/amazon-kindle-and-fire-tablets-15">Need a Kindle?</a>
+        <a href="https://www.woot.com/category/computers/tablets">Need a Kindle?</a>
       </span>
       <span className="text-sm font-extralight text-primary-100">
         <a href="http://bookshelfadmin.jk">Admin</a>
